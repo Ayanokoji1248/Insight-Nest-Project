d6dadae1910c0bec763ed12dfe6bd98e2fdff5a0
Krish <krishprajapati1248@gmail.com>
2025-06-26 20:47:32 +0530

    edit blog

--- server/src/controllers/blog.controller.ts
+++ server/src/controllers/blog.controller.ts
@@ -73,3 +73,80 @@ export const createBlog = async (req: Request, res: Response, next: NextFunction
     }
 }
 
+
+export const editBlog = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user;
+        const { id } = req.params
+        const { title, content, image, tags } = req.body
+
+        const validate = blogSchema.safeParse(req.body);
+
+        if (!validate.success) {
+            res.status(400).json({
+                errors: validate.error.flatten().fieldErrors
+            })
+            return
+        }
+
+        if (!userId) {
+            res.status(400).json({
+                message: "Unauthorized"
+            })
+            return
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            res.status(400).json({
+                message: "Mongoose Validation Error"
+            })
+            return
+        }
+
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            res.status(404).json({
+                message: "User Not Found"
+            })
+            return
+        }
+
+        const blogExist = await blogModel.findById(id)
+
+        if (!blogExist) {
+            res.status(400).json({
+                message: "Blog doesnt exist"
+            })
+            return
+        }
+
+        if (blogExist?.user.toString() !== userId.toString()) {
+            res.status(400).json({
+                message: "Invalid Blog"
+            })
+            return
+        }
+
+        const blogEdit = await blogModel.findByIdAndUpdate(id, {
+            title,
+            content,
+            image,
+            tags
+        }, { new: true });
+
+        res.status(200).json({
+            message: "Blog Edited",
+            blog: blogEdit
+        })
+        return
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
--- server/src/routes/blog.route.ts
+++ server/src/routes/blog.route.ts
@@ -1,8 +1,16 @@
 import { Router } from "express"
 import { userMiddleware } from "../middlewares/user.middleware";
-import { createBlog } from "../controllers/blog.controller";
+import { createBlog, editBlog } from "../controllers/blog.controller";
 const blogRouter = Router();
 
 blogRouter.post('/create', userMiddleware, createBlog)
+
+blogRouter.get('/:id', userMiddleware,)
+
+blogRouter.get('/all', userMiddleware,)
+
+blogRouter.put('/edit/:id', userMiddleware, editBlog)
+
+blogRouter.delete('/delete/:id', userMiddleware,)
 
 export default blogRouter
