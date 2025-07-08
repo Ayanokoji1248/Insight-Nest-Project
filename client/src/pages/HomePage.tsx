cb3f09ff31e668d8efd8b8a6c941c58fdc23816f
Krish <krishprajapati1248@gmail.com>
2025-07-08 10:12:40 +0530

    homepage addded

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import FormModal from "../components/FormModal"
 import NavBar from "../components/NavBar"
 import userModalStore from "../store/userModalStore"
